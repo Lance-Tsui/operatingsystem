6114b1cb5e132ba613fe67aad8552fbdce859fff
s275xu <s275xu@uwaterloo.ca>
2022-05-09 16:26:35 -0400

    a0 q2

--- kern/startup/menu.c
+++ kern/startup/menu.c
@@ -274,6 +274,18 @@ cmd_panic(int nargs, char **args)
 }
 
 /*
+ * Command for debugging threads
+ */
+ static
+ int
+ cmd_dth(int nargs, char **args){
+	 (void)nargs;
+	 (void)args;
+	 dbflags = DB_THREADS;
+	 return 0;
+ }
+
+/*
  * Command for shutting down.
  */
 static
@@ -436,6 +448,7 @@ static const char *opsmenu[] = {
 	"[pwd]     Print current directory   ",
 	"[sync]    Sync filesystems          ",
 	"[panic]   Intentional panic         ",
+	"[dth]	   Thread Debugging			 ",
 	"[q]       Quit and shut down        ",
 	NULL
 };
@@ -546,6 +559,7 @@ static struct {
 	{ "pwd",	cmd_pwd },
 	{ "sync",	cmd_sync },
 	{ "panic",	cmd_panic },
+	{"dth",		cmd_dth},
 	{ "q",		cmd_quit },
 	{ "exit",	cmd_quit },
 	{ "halt",	cmd_quit },
